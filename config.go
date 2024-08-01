49279cf3a3f4ddcfc418b29576a2403f3e842c36
Alex Story <astory11@yahoo.com>
2024-07-31 22:55:42 -0500

    initial commit

---
+++ config.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/BurntSushi/toml"
+)
+
+var re = regexp.MustCompile(`'[^']*'|"[^"]*"|\S+`)
+
+type Command struct {
+	Name      string
+	Command   string
+	Arguments []string
+}
+
+type Config struct {
+	Commands []Command
+}
+
+func parseConfig(data map[string]interface{}) Config {
+	var config Config
+
+	if commands, ok := data["commands"].(map[string]interface{}); ok {
+		for name, cmd := range commands {
+			switch cmd := cmd.(type) {
+			case string:
+				command := splitCommand(cmd)[0]
+				args := splitCommand(cmd)[1:]
+				config.Commands = append(config.Commands, Command{
+					Name:      name,
+					Command:   command,
+					Arguments: args,
+				})
+			case map[string]interface{}:
+				command := Command{Name: name}
+				if cmdStr, ok := cmd["command"].(string); ok {
+					command.Command = cmdStr
+				}
+				if args, ok := cmd["args"].([]interface{}); ok {
+					for _, arg := range args {
+						if argStr, ok := arg.(string); ok {
+							command.Arguments = append(command.Arguments, argStr)
+						}
+					}
+				}
+				config.Commands = append(config.Commands, command)
+			}
+		}
+	}
+
+	return config
+}
+
+func loadConfig() Config {
+	configPath, err := findConfigFile()
+	if err != nil {
+		fmt.Println("config file not found")
+		os.Exit(1)
+	}
+
+	var data map[string]interface{}
+	if _, err := toml.DecodeFile(configPath, &data); err != nil {
+		fmt.Println("failed to parse config file:", err)
+		os.Exit(1)
+	}
+
+	return parseConfig(data)
+}
+
+func findConfigFile() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	for {
+		configPath := filepath.Join(dir, "gr.toml")
+		if _, err := os.Stat(configPath); err == nil {
+			return configPath, nil
+		}
+
+		parentDir := filepath.Dir(dir)
+		if parentDir == dir {
+			break
+		}
+		dir = parentDir
+	}
+
+	return "", fmt.Errorf("config file not found")
+}
+
+func splitCommand(cmd string) []string {
+	return re.FindAllString(cmd, -1)
+}
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type options struct {
+	quiet bool
+}
+
+func main() {
+	var quiet bool
+
+	flag.BoolVar(&quiet, "q", false, "suppress output")
+	flag.BoolVar(&quiet, "quiet", false, "suppress output")
+
+	flag.Usage = helpCmd
+
+	flag.Parse()
+	opts := options{quiet: quiet}
+	cmd := flag.Arg(0)
+
+	switch cmd {
+	case "list":
+		listCmd()
+	case "", "help":
+		helpCmd()
+	default:
+		runCmd(cmd, opts)
+	}
+}
+
+func helpCmd() {
+	fmt.Println("Usage: gr [options] <command>")
+	fmt.Println("Commands:")
+	fmt.Println("  help  prints this message")
+	fmt.Println("  list  lists all available commans")
+	fmt.Println("\nOptions:")
+	fmt.Println("  -q, --quiet  suppress output")
+}
+
+func listCmd() {
+	config := loadConfig()
+
+	for _, task := range config.Commands {
+		fmt.Println(task.Name)
+	}
+}
+
+func runCmd(cmd string, opts options) {
+	config := loadConfig()
+
+	for _, task := range config.Commands {
+		if task.Name == cmd {
+			runTask(task, opts)
+			return
+		}
+	}
+
+	fmt.Printf("Command %q not found\n", cmd)
+}
+
+func runTask(task Command, opts options) {
+	cmd := exec.Command(task.Command, task.Arguments...)
+	if !opts.quiet {
+		fmt.Printf("Running %s %v...\n\n", task.Command, strings.Join(task.Arguments, ""))
+		cmd.Stdout = os.Stdout
+	} else {
+		cmd.Stdout = nil
+	}
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+}
